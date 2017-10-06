38bfec2ff8e7b4e42f2ed701e236fbe84efae739
kmeda <karthikreddymeda@gmail.com>
2017-10-06 23:18:24 +0530

    calc functionality added

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,216 +1,116 @@
+
 $(document).ready(function() {
 
-  var entry = '';
-  var ans = '';
-  var current = '';
-  var log = '';
-  var decimal = true;
-  var reset = '';
+    var mathOperands=["+","-","/","*"];
+    var dotOperand=["."];
+    var digits=[0,1,2,3,4,5,6,7,8,9];
+    var decimal = true;
+    var values=[''];
+    var evalInputs;
+    var total;
 
-  // round function if answer includes a decimal
-  function round(val) {
-    val = val.toString().split('');
-    if (val.indexOf('.') !== -1) {
-      var valTest = val.slice(val.indexOf('.') + 1, val.length);
-      val = val.slice(0, val.indexOf('.') + 1);
-      var i = 0;
-      while (valTest[i] < 1) {
-        i++
+   var getEntry = function(input){
+
+     if(dotOperand.includes(values[values.length-1]) && input==="."){
+       return;
+     }
+     else if(values.length===1 && !mathOperands.includes(values)){
+       console.log("TEST for initial operator");
+       if (input === '.') {
+         decimal = false;
+       }
+       values.push(input);
+
+     }
+     else if (!mathOperands.includes(values[values.length-1])){
+       console.log("TEST for an operator");
+
+       if (input === '.' && decimal === false) {
+         return;
+       }
+       if (input === '.') {
+         decimal = false;
+       }
+       values.push(input);
+     }
+     else if (digits.includes(Number(input))){
+       console.log("TEST for a number");
+       decimal = true;
+       values.push(input);
+     }
+
+     update();
+   }
+
+    var update = function(){
+      if (values.length === 0) {
+        values = [''];
       }
-      valTest = valTest.join('').slice(0, i + 2);
-      if (valTest[valTest.length-1] === '0') {
-        valTest = valTest.slice(0, -1);
+
+      evalInputs=values.join("");
+
+      if (evalInputs.length === 0) {
+        $(".main-screen").html('0');
+        return;
       }
-      return val.join('') + valTest;
-    } else {
-      return val.join('');
+
+       if(evalInputs.length > 20){
+
+         evalInputs=["Digit Limit Reached"];
+
+      }
+
+    $(".main-screen").html(evalInputs);
+
     }
-  }
 
-  $('button').click(function() {
 
-    entry = $(this).attr("value");
-    console.log('entry: ' + entry);
+    var getResult = function(){
+      evalInputs=values.join("");
+      var result = (eval(evalInputs)).toFixed(2);
+      total = result;
+      // if result contains decimal , round off to 2 digits
+      console.log(result.toString());
 
-    //reset for log after answer to equation.
-    if (reset) {
-      if (entry === '/' || entry === '*' || entry === '-' || entry === '+' || entry === '%') {
-        log = ans;
-      } else {
-        ans = '';
-      }
+      $(".main-screen").html(result);
+
     }
-    reset = false;
 
-    // All clear or Clear Entry
-    if (entry === 'ac' || entry === 'ce' && current === 'noChange') {
-      ans = '';
-      current = '';
-      entry = '';
-      log = '';
-      $('.chaining').html('0');
-      $('.main-screen').html('0');
+    var getPercentile = function() {
+      total /=100;
+      $(".main-screen").html(total);
+    }
+
+    $("button").on("click",function(){
+      var entry = $(this).attr("value");
+
+     if(entry === "ac"){
+      values=[""];
       decimal = true;
-    } else if (entry === 'ce') {
-      $('.chaining').html(log.slice(0, -current.length));
-      log = log.slice(0, -current.length);
-      ans = ans.slice(0, -current.length);
-      current = ans;
-      if (log.length === 0 || log === ' ') {
-        $('.chaining').html('0');
+      update();
       }
-      $('.main-screen').html('0');
-      entry = '';
-      decimal = true;
-    }
-
-    // prevents more than one deciminal in a number
-    if (entry === '.' || entry === '0.') {
-      if (!decimal) {
-        entry = '';
+      else if(entry==="ce"){
+        values.pop();
+        update();
       }
-    }
-
-    // prevents improper use of first digit
-    if (ans.length === 0 && isNaN(entry) && entry !== '.' || ans.length === 0 && entry === '0') {
-      entry = '';
-      ans = '';
-    }
-
-    // prevents extra operators
-    if (current !== 'noChange') {
-      if (current === '' && isNaN(entry) && entry !== '.' || isNaN(current) && isNaN(entry) && entry !== '.') {
-        entry = '';
+      else if (entry==="="){
+        getResult();
       }
-    }
-
-    // digit combining
-    while (Number(entry) || entry === '0' || current === '.') {
-
-      if (isNaN(current) && entry === '0' && current !== '.') {
-        entry = '';
-      } else if (isNaN(current) && Number(entry) && current !== '.') {
-        current = '';
+      else if (entry==="%"){
+        getResult();
+        getPercentile();
       }
-      if (entry === '.') {
-        decimal = false;
-      }
-      if (current === '0.' && isNaN(entry)) {
-        entry = '';
-      } else {
-        if (current[current.length - 1] === '.') {
-          current = current.concat(entry);
-        } else {
-          current += entry;
+
+      else{
+        if(values[values.length-1].indexOf("+","-","/","*",".")===-1){
+          console.log("TEST for operators");
+          getEntry(entry);
         }
-        ans += entry;
-        $('.main-screen').html(current);
-        log += entry;
-        $('.chaining').html(log);
-        entry = '';
+      else{
+        getEntry(entry);
       }
     }
 
-    // Operation list
+    });
 
-    if (entry === '.') {
-      if (current === '' || isNaN(current[current.length - 1])) {
-        current = '0.';
-        ans += entry;
-        $('.main-screen').html('0.');
-        log += current;
-        $('.chaining').html(log);
-
-      } else {
-        current = current.concat('.');
-        ans = ans.concat('.');
-        log = ans;
-        $('.chaining').html(ans);
-        $('.main-screen').html(current);
-      }
-      entry = '';
-      decimal = false;
-
-    } else if (entry === '/') {
-      current = '/';
-      ans = round(eval(ans)) + current;
-      log += current;
-      $('.chaining').html(log);
-      $('.main-screen').html('/');
-      entry = '';
-      decimal = true;
-
-    } else if (entry === '*') {
-      current = '*';
-      ans = round(eval(ans)) + current;
-      log += 'x';
-      $('.chaining').html(log);
-      $('.main-screen').html('x');
-      entry = '';
-      decimal = true;
-
-    } else if (entry === '-') {
-      current = '-';
-      ans = round(eval(ans)) + current;
-      log += current;
-      $('.chaining').html(log);
-      $('.main-screen').html('-');
-      entry = '';
-      decimal = true;
-
-    } else if (entry === '+') {
-      current = '+';
-      ans = round(eval(ans)) + current;
-      log += current;
-      $('.chaining').html(log);
-      $('.main-screen').html('+');
-      entry = '';
-      decimal = true;
-
-    } else if (entry === '%') { //Percentile mod
-      current = '%';
-      ans /=100;
-      log = ans;
-      $('.chaining').html(log);
-      $('.main-screen').html('%');
-      entry = '';
-      decimal = true;
-
-    }else if (entry === '=') {
-      if (current[current.length - 1] === '.') {
-        entry = '';
-      } else {
-        current = eval(ans).toString();
-        $('.main-screen').html(round(eval(ans)));
-        ans = round(eval(ans));
-        log += entry + ans;
-        $('.chaining').html(log);
-        log = ans;
-        entry = '';
-        reset = true;
-        decimal = true;
-      }
-      current = 'noChange';
-    }
-    entry = '';
-
-    if (reset) {
-      log = '';
-    }
-
-    // max digits on screen
-    if ($('.main-screen').text().length > 8 || $('.chaining').text().length > 33) {
-      $('.main-screen').html('0');
-      $('.chaining').html('Digit Limit Met');
-      current = '';
-      ans = '';
-      log = '';
-      decimal = true;
-    }
-
-    console.log('decimal: ' + decimal);
-    console.log('current: ' + current);
-    console.log('answer: ' + ans);
-    console.log($('.chaining').text().length);
-  });
 });
